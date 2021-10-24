d4f0dfd5db5a310a3868328c115743efb4512019
Invictus-ill <therohanm@gmail.com>
2021-10-25 00:01:09 +0530

    Trying different forward function

--- model/modeling_jointmbart.py
+++ model/modeling_jointmbart.py
@@ -11,7 +11,7 @@ class JointMBart(MBartPreTrainedModel):
         self.args = args
         self.num_intent_labels = len(intent_label_lst)
         self.num_slot_labels = len(slot_label_lst)
-        self.bert = MBartModel(config=config)  # Load pretrained bert
+        self.mbart = MBartModel(config=config)  # Load pretrained bert
 
         self.intent_classifier = IntentClassifier(config.hidden_size, self.num_intent_labels, args.dropout_rate)
         self.slot_classifier = SlotClassifier(config.hidden_size, self.num_slot_labels, args.dropout_rate)
@@ -19,11 +19,10 @@ class JointMBart(MBartPreTrainedModel):
         if args.use_crf:
             self.crf = CRF(num_tags=self.num_slot_labels, batch_first=True)
 
-    def forward(self, input_ids, attention_mask, token_type_ids, intent_label_ids, slot_labels_ids):
-        outputs = self.bert(input_ids, attention_mask=attention_mask,
-                            token_type_ids=token_type_ids)  # sequence_output, pooled_output, (hidden_states), (attentions)
+    def forward(self, input_ids, attention_mask, intent_label_ids, slot_labels_ids):
+        outputs = self.mbart(input_ids, attention_mask=attention_mask)  # last-layer hidden-state, (hidden_states), (attentions)
         sequence_output = outputs[0]
-        pooled_output = outputs[1]  # [CLS]
+        pooled_output = sequence_output[:, 0]  # [CLS]
 
         intent_logits = self.intent_classifier(pooled_output)
         slot_logits = self.slot_classifier(sequence_output)
@@ -56,7 +55,7 @@ class JointMBart(MBartPreTrainedModel):
                     slot_loss = slot_loss_fct(slot_logits.view(-1, self.num_slot_labels), slot_labels_ids.view(-1))
             total_loss += self.args.slot_loss_coef * slot_loss
 
-        outputs = ((intent_logits, slot_logits),) + outputs[2:]  # add hidden states and attention if they are here
+        outputs = ((intent_logits, slot_logits),) + outputs[1:]  # add hidden states and attention if they are here
 
         outputs = (total_loss,) + outputs
 
