f04e3d5bb966d712b4600304460918a9f209f7f1
Rohit Sadavarte <sadavarte.rohit851@gmail.com>
2021-10-25 11:07:08 +0530

    adding dev

--- data/atis/train/test.py
+++ data/atis/train/test.py
@@ -1,16 +1,30 @@
-a = open('seq.out')
-b = open('seq.in')
+import json
 
-al = a.readlines()
-bl = b.readlines()
+f = open('file.json',)
+lines = f.readlines()
 
-
-for i in range(len(al)):
-    if (not len(al[i].split())==len(bl[i].split())):
-        print(i,len(al[i].split()),len(bl[i].split()))
-        print(al[i].split())
-        print(bl[i].split())
+seqin = open('seq.in','w')
+seqout = open('seq.out','w')
+label = open('label','w')
 
 
-a.close()
-b.close()
+for i in lines[1000:]:
+    a = json.loads(i)
+    if('code_mixed_sentence' in a):
+        slots = str(a["code_mixed_slots"]).strip().replace(',',' ')+ "\n"
+        sentence = str(a['code_mixed_sentence']).strip()+ "\n"
+        l = str(a['intents']).strip() + "\n"
+
+        if(len(slots.split()) == len(sentence.split())): 
+
+            seqin.write(sentence)
+            seqout.write(slots)
+            label.write(l)
+
+seqin.close()
+seqout.close()
+label.close()
+
+    
+
+f.close()
