6e422858ae4e5fc7d98a216a2a2185a4ca7209e9
Rohit Sadavarte <sadavarte.rohit851@gmail.com>
2021-10-25 11:02:48 +0530

    corrections to last commit

--- data/atis/train/test.py
+++ data/atis/train/test.py
@@ -1,14 +1,16 @@
 a = open('seq.out')
+b = open('seq.in')
 
-text = a.read()
+al = a.readlines()
+bl = b.readlines()
+
+
+for i in range(len(al)):
+    if (not len(al[i].split())==len(bl[i].split())):
+        print(i,len(al[i].split()),len(bl[i].split()))
+        print(al[i].split())
+        print(bl[i].split())
+
 
 a.close()
-
-a=open('seq.out', 'w')
-
-text = text.replace(',', ' ')
-
-a.write(text)
-
-a.close()
-
+b.close()
