0b68c00f3780925cf4a4ec4fde58be9c577c0587
Invictus-ill <77726890+Invictus-ill@users.noreply.github.com>
2022-01-07 16:06:23 +0530

    Output Format Change
    Removed the intent, kept IOB slot output

--- predict.py
+++ predict.py
@@ -197,14 +197,17 @@ def predict(pred_config):
 
     # Write to output file
     with open(pred_config.output_file, "w", encoding="utf-8") as f:
+        print(slot_preds_list)
         for words, slot_preds, intent_pred in zip(lines, slot_preds_list, intent_preds):
+            
             line = ""
             for word, pred in zip(words, slot_preds):
                 if pred == 'O':
-                    line = line + word + " "
+                    line = line + "O,"
                 else:
-                    line = line + "[{}:{}] ".format(word, pred)
-            f.write("<{}> -> {}\n".format(intent_label_lst[intent_pred], line.strip()))
+                    line = line + pred +","
+            line = line[:-1]
+            f.write("{}\n".format(line.strip()))
 
     logger.info("Prediction Done!")
 
@@ -215,7 +218,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--input_file", default="sample_pred_in.txt", type=str, help="Input file for prediction")
     parser.add_argument("--output_file", default="sample_pred_out.txt", type=str, help="Output file for prediction")
-    parser.add_argument("--model_dir", default="./atis_model", type=str, help="Path to save, load model")
+    parser.add_argument("--model_dir", default="../atis_model", type=str, help="Path to save, load model")
 
     parser.add_argument("--batch_size", default=32, type=int, help="Batch size for prediction")
     parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
